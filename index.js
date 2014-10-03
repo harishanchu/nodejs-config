6ad32db1fe98d1fdd19b9936a6aa3fb05a020a05
Harish <harishanchu@gmail.com>
2014-10-03 22:35:17 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Index.js
+ *
+ * @auther: harish <twitter:@harishanchu>
+ * Copyright (c) 2014, Harish Anchu. All rights reserved.
+ */
+
+var Config = require('./lib/config/Config');
+var FileLoader = require('./lib/config/FileLoader');
+var environmentDetector = require('./lib/config/environmentDetector');
+var path = require('path');
+var helper = require('./lib/support/helper');
+
+module.exports = function (environments, configDirPath) {
+    if (!helper.isset(environments))
+        environments = {};
+
+    if (!helper.isset(configDirPath))
+        configDirPath = "";
+
+    var env = process.env.NODE_ENV = environmentDetector.detect(environments);
+
+    return new Config(new FileLoader(path.join(configDirPath, 'config')), env)
+};
