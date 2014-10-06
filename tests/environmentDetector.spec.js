0bc2d8a92c350c757d64bee12888f58d1aeb0b13
Harish <harishanchu@gmail.com>
2014-10-06 23:37:02 +0530

    Code reformat

--- tests/environmentDetector.spec.js
+++ tests/environmentDetector.spec.js
@@ -5,20 +5,20 @@
  * Copyright (c) 2014, Harish Anchu. All rights reserved.
  */
 
-describe("environmentDetector", function(){
-    it("should be an object.", function(){
+describe("environmentDetector", function () {
+    it("should be an object.", function () {
         var environmentDetecor = require('../lib/config/environmentDetector')
         expect(typeof environmentDetecor).toEqual('object');
         expect(typeof environmentDetecor.detect).toEqual("function");
     });
 
-    describe("should have a method detect and", function(){
-        it("should be callabe", function(){
+    describe("should have a method detect and", function () {
+        it("should be callabe", function () {
             var environmentDetecor = require('../lib/config/environmentDetector')
             expect(typeof environmentDetecor.detect).toEqual("function");
         })
 
-        it("should return an environment name which this running machine belongs to.", function(){
+        it("should return an environment name which this running machine belongs to.", function () {
             var os = require('os');
             var environmentDetecor = require('../lib/config/environmentDetector');
             expect(environmentDetecor.detect(
@@ -29,11 +29,11 @@ describe("environmentDetector", function(){
             )).toEqual("development");
         });
 
-        it("should return 'production' as environment there is no environment specified with running machine.", function(){
+        it("should return 'production' as environment there is no environment specified with running machine.", function () {
             var environmentDetecor = require('../lib/config/environmentDetector');
             expect(environmentDetecor.detect(
                 {
-                    development: ['sfhshdfhhskd','sdsa'],
+                    development: ['sfhshdfhhskd', 'sdsa'],
                     production: ['hfhhsdjjsd']
                 }
             )).toEqual("production");
@@ -41,9 +41,11 @@ describe("environmentDetector", function(){
             expect(environmentDetecor.detect({})).toEqual("production");
         });
 
-        it("should return response of callback method provided as environment.", function(){
+        it("should return response of callback method provided as environment.", function () {
             var environmentDetecor = require('../lib/config/environmentDetector');
-            expect(environmentDetecor.detect(function(){ return "myEnvlogic"})).toEqual("myEnvlogic");
+            expect(environmentDetecor.detect(function () {
+                return "myEnvlogic"
+            })).toEqual("myEnvlogic");
         });
     });
 })
